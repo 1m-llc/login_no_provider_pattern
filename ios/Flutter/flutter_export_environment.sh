db1e112c1bf5fb45d4157e42d2f37a351e8905b4
imoto <imoto.hideyuki.pv@gmail.com>
2020-11-07 18:25:10 +0900

    初期コミット

---
+++ ios/Flutter/flutter_export_environment.sh
@@ -0,0 +1,16 @@
+#!/bin/sh
+# This is a generated file; do not edit or check into version control.
+export "FLUTTER_ROOT=/Users/imoto/Documents/development/flutter/flutter"
+export "FLUTTER_APPLICATION_PATH=/Users/imoto/development/1m/no_provider_sample"
+export "FLUTTER_TARGET=/Users/imoto/development/1m/no_provider_sample/lib/main.dart"
+export "FLUTTER_BUILD_DIR=build"
+export "SYMROOT=${SOURCE_ROOT}/../build/ios"
+export "OTHER_LDFLAGS=$(inherited) -framework Flutter"
+export "FLUTTER_FRAMEWORK_DIR=/Users/imoto/Documents/development/flutter/flutter/bin/cache/artifacts/engine/ios"
+export "FLUTTER_BUILD_NAME=1.0.0"
+export "FLUTTER_BUILD_NUMBER=1"
+export "DART_DEFINES=flutter.inspector.structuredErrors%3Dtrue"
+export "DART_OBFUSCATION=false"
+export "TRACK_WIDGET_CREATION=true"
+export "TREE_SHAKE_ICONS=false"
+export "PACKAGE_CONFIG=.packages"
